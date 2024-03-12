11505b87454eb7e4d653c893b4fa88de1e3a450d
NextCodigos <nebulanemesis372@gmail.com>
2024-03-12 01:10:45 +0100

    Haciendo componente vue3 de dos archivos haciendo en astro en un componente,ne falla mirar

---
+++ src/components/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Padre:</h1>
+        <!-- El padre envía al hijo estos dos valores -->
+        <HijoVue :nombre="nombre" :edad="edad" @modificar="cambiarValor" />
+    </div>
+
+    <div>
+        <h1>HijoVue:</h1>
+        <h2>Te llamas {{ nombre }} y tienes {{ edad }} años</h2>
+        <button @click="enviar">Cambiar Valores</button>
+    </div>
+</template>
+
+<script setup>
+import { defineEmits, defineProps, ref } from 'vue';
+
+// Padre
+
+
+
+
+// Hijo
+const cambiarValor = (valor1, valor2) => {
+    nombre.value = valor1;
+    edad.value = valor2;
+};
+
+const nombre = ref('JAB');
+const edad = ref(20);
+
+const emit = defineEmits(['modificar']);
+const enviar = () => {
+    emit('modificar', nombre.value, edad.value);
+};
+
+const props = defineProps({
+    nombre: String,
+    edad: Number,
+});
+</script>
+
+<style></style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>HijoVue:</h1>
+        <h2>{{nombre}} y tienes {{edad}} años</h2>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    nombre: String,
+    edad: Number,
+})
+</script>
